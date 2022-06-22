ffcf8303f5196f726c8737d365d98de1839e62e5
iannbraga <bragaprestes@gmail.com>
2022-06-22 20:10:47 -0300

    commit automatico

--- salvarNoGit.sh
+++ salvarNoGit.sh
@@ -1,6 +1,15 @@
 #!/bash/bin
 
+echo "Adicionando arquivos"
 git add .
-git commit -m "salvandoAutomatico"
+sleep 2
+echo "Realizando commit"
+git commit -m "commit automatico"
+echo "Seleciona branch main"
 git branch -M main
+echo "Faz o push dos arquivos"
 git push -u origin main
+sleep 2
+echo "Encerrando"
+sleep 2
+echo "Adeus"
